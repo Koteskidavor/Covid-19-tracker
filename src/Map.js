a15bf8e909bb27c1ff88cfddfc24fc011cd9336a
Unbreaks <51781170+Unbreaks@users.noreply.github.com>
2020-08-27 17:54:30 +0200

    Add files via upload

---
+++ src/Map.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Map as LeafletMap, TileLayer} from "react-leaflet";
+import './map.css'
+import { showDataonMap } from "./util"
+
+
+function Map({countries, center, zoom, casesType}) {
+    return (
+        <div className="map">
+            <LeafletMap center={center} zoom={zoom}>
+                <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenSteetMap</a> contributors'/>
+            {/* Loop and draw circles */}
+
+
+              {showDataonMap(countries, casesType)}
+            </LeafletMap>
+            
+        </div>
+    )
+}
+
+export default Map;
+
